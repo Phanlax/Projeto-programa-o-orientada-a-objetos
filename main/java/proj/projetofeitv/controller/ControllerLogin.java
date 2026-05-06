9faaf72b5b9ebe585bcbe3b90bf1a8337dcdfe16
Gustavo de Oliveira Santos <gustavo.os170205@gmail.com>
2026-05-06 15:49:52 -0300

    Refactor login method to use Usuario model e olhar o id

--- main/java/proj/projetofeitv/controller/ControllerLogin.java
+++ main/java/proj/projetofeitv/controller/ControllerLogin.java
@@ -7,6 +7,7 @@ package proj.projetofeitv.controller;
 import java.sql.Connection;
 import proj.projetofeitv.dao.Conexao;
 import proj.projetofeitv.dao.UsuarioDAO;
+import proj.projetofeitv.model.Usuario;
 import proj.projetofeitv.view.TelaLogin;
 
 /**
@@ -24,30 +25,36 @@ public class ControllerLogin {
 
     public void login() {
 
-        String usuario = view.getUsuario();
-        String senha = view.getSenha();
+    String usuario = view.getUsuario();
+    String senha = view.getSenha();
 
-        if (usuario.isEmpty() || senha.isEmpty()) {
-            view.mostrarMensagem("Preencha todos os campos!");
-            return;
-        }
-
-        try (Connection conn = new Conexao().getConnection()) {
-
-            UsuarioDAO dao = new UsuarioDAO(conn);
-
-            boolean existe = dao.existeUsuario(usuario, senha);
-
-            if (existe) {
-                view.mostrarMensagem("Login realizado com sucesso!");
-                nav.abrirPrincipal(); 
-            } else {
-                view.mostrarMensagem("Usuário ou senha inválidos!");
-            }
-
-        } catch (Exception e) {
-            view.mostrarMensagem("Erro ao fazer login");
-            e.printStackTrace();
-        }
+    if (usuario.isEmpty() || senha.isEmpty()) {
+        view.mostrarMensagem("Preencha todos os campos!");
+        return;
     }
+
+    try (Connection conn = new Conexao().getConnection()) {
+
+        UsuarioDAO dao = new UsuarioDAO(conn);
+
+        Usuario usuarioLogado = dao.login(usuario, senha); 
+
+        if (usuarioLogado != null) {
+
+            view.mostrarMensagem("Login realizado com sucesso!");
+
+            
+            int id = usuarioLogado.getId();
+
+            nav.abrirPrincipal();
+
+        } else {
+            view.mostrarMensagem("Usuário ou senha inválidos!");
+        }
+
+    } catch (Exception e) {
+        view.mostrarMensagem("Erro ao fazer login");
+        e.printStackTrace();
+    }
+  }
 }
