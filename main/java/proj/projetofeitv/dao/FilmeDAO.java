98acb7b00be5b91dc3b303444b7cfb2f01b6922d
Gustavo de Oliveira Santos <gustavo.os170205@gmail.com>
2026-05-06 15:47:23 -0300

    adicionei a busca do banco de dados dos filmes

---
+++ main/java/proj/projetofeitv/dao/FilmeDAO.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proj.projetofeitv.dao;
+
+import java.sql.Connection;
+import java.util.List;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import proj.projetofeitv.model.Filme;
+
+/**
+ *
+ * @author Gustavo
+ */
+public class FilmeDAO {
+    private Connection conn;
+
+public FilmeDAO(Connection conn) {
+    this.conn = conn;
+}
+    
+    public List<Filme> listarFilmes() throws SQLException {
+    List<Filme> lista = new ArrayList<>();
+
+    String sql = "SELECT * FROM filmes";
+
+    PreparedStatement stmt = conn.prepareStatement(sql);
+    ResultSet rs = stmt.executeQuery();
+
+    while (rs.next()) {
+        Filme f = new Filme();
+        f.setTitulo(rs.getString("titulo"));
+        f.setGenero(rs.getString("genero"));
+        f.setDuracao(rs.getString("duracao"));
+        f.setAno(rs.getInt("ano"));
+
+        lista.add(f);
+    }
+
+    return lista;
+}
+}
